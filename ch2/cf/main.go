387539605f6fc643aea395d675f52ee763f20572
Joshua Kaplan <yhkaplan@gmail.com>
2018-02-24 18:36:15 +0900

    Made temp conversion util

---
+++ ch2/cf/main.go
@@ -0,0 +1,24 @@
+// CF converts numeric arguments to C and F
+package main
+
+import (
+	"The-Go-Programming-Language/ch2/tempconv"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	for _, arg := range os.Args[1:] {
+		t, err := strconv.ParseFloat(arg, 64)
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "cf: %v\n", err)
+			os.Exit(1)
+		}
+
+		f := tempconv.Fahrenheit(t)
+		c := tempconv.Celsius(t)
+		fmt.Printf("%s = %s, %s = %s\n", f, tempconv.FToC(f), c, tempconv.CToF(c))
+	}
+}
