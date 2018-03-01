658cb7456d76da505c0196fa7fec467bc7715c92
Joshua Kaplan <yhkaplan@gmail.com>
2018-03-01 23:20:59 +0900

    Added another package

---
+++ ch4/equal/equal.go
@@ -0,0 +1,14 @@
+package equal
+
+func Equal(x, y []string) bool {
+	if len(x) != len(y) {
+		return false
+	}
+
+	for i := range x {
+		if x[i] != y[i] {
+			return false
+		}
+	}
+	return true
+}
--- ch4/slice_tests/main.go
+++ ch4/slice_tests/main.go
@@ -8,6 +8,14 @@ import (
 
 func main() {
 	a := [...]int{0, 1, 2, 3, 4, 5}
+	// Calling reverse once just reverses
+	rev.Reverse(a[:])
+	fmt.Println(a)
+
+	// However, calling it as below "rotates" the
+	// slice by several positions
+	rev.Reverse(a[:2])
+	rev.Reverse(a[2:])
 	rev.Reverse(a[:])
 	fmt.Println(a)
 }
